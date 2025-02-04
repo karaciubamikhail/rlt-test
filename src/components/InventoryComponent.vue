439fd5d18d465d79fe818c8de7040a8d66270246
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2025-02-04 09:49:51 +0300

    Create enventar

---
+++ src/components/InventoryComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="inventory">
+      <img src="../../src/assets/images/Img.png" alt="" class="inventory__image">
+      <div class="item">
+        <div class="item__title"></div>
+        <div class="item__descr"></div>
+        <div class="item__descr"></div>
+        <div class="item__descr"></div>
+        <div class="item__descr"></div>
+        <div class="item__descr"></div>
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+.inventory {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 236px;
+  background-color: #262626;
+  padding: 18px 14px 0 14px;
+  border: 1px solid #4D4D4D;
+  border-radius: 12px;
+
+  &__image {
+    margin-bottom: 20px;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+//@ts-ignore
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
