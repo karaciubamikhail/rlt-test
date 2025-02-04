d0b3b575817daae8da49cc52135ae66fc1d788bb
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2025-02-04 10:41:37 +0300

    Change pachage

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-    publicPath: '/rlt-test/'
-  }
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
+    transpileDependencies: true,
+    publicPath: (process.env.NODE_ENV === "production") ? "/kodakriti/" : "/"
+});
